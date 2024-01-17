42767211e1d1b68470a73084e6eb41c1893ddccf
Wolffkran <liebherr2008@live.com>
2024-01-17 16:53:15 -0500

    .env

--- config/config.js
+++ config/config.js
@@ -6,7 +6,6 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   // If JAWSDB_URL is present (Heroku), use it
   sequelize = new Sequelize(process.env.JAWSDB_URL, {
-    logging: console.log, // Log all SQL queries to console
     dialect: 'mysql',
     dialectOptions: {
       ssl: {
@@ -20,7 +19,6 @@ if (process.env.JAWSDB_URL) {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306,
-    logging: console.log, // Log all SQL queries to console
   });
 }
 
