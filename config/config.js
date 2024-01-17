88b7f539a2f30b170f82a34a237f0d278eb96c49
Wolffkran <liebherr2008@live.com>
2024-01-17 17:16:59 -0500

    config

--- config/config.js
+++ config/config.js
@@ -1,10 +1,6 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
-
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  // If JAWSDB_URL is present (Heroku), use it
   sequelize = new Sequelize(process.env.JAWSDB_URL, {
     dialect: 'mysql',
     dialectOptions: {
@@ -12,15 +8,13 @@ if (process.env.JAWSDB_URL) {
         rejectUnauthorized: false, // For MySQL on Heroku
       },
     },
-    logging: false, 
   });
 } else {
-  // Use local database configuration
+  // Use local configuration or whatever is appropriate for development
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3001,
-    logging: console.log, // Log all SQL queries to console for development
+    port: 3306,
   });
 }
 
