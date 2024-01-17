65495ffb3bb7d0c170f62441327cf74c42c3c0c7
Wolffkran <liebherr2008@live.com>
2024-01-17 16:35:25 -0500

    config

--- config/config.js
+++ config/config.js
@@ -1,16 +1,27 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// create connection to our db
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL, {
-      logging: console.log, // Log all SQL queries to console
-    })
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-      logging: console.log, // Log all SQL queries to console
-    });
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  // If JAWSDB_URL is present (Heroku), use it
+  sequelize = new Sequelize(process.env.JAWSDB_URL, {
+    logging: console.log, // Log all SQL queries to console
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false, // For MySQL on Heroku
+      },
+    },
+  });
+} else {
+  // Use local database configuration
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+    logging: console.log, // Log all SQL queries to console
+  });
+}
 
 module.exports = sequelize;
