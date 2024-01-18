68ed325af2576be5807cf58bf6b14cd40a20e708
Wolffkran <liebherr2008@live.com>
2024-01-18 00:28:18 -0500

    Heroku

--- config/config.js
+++ config/config.js
@@ -14,9 +14,9 @@ if (process.env.JAWSDB_URL) {
   });
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
+    host: process.env.DB_HOST || 'localhost', // Use DB_HOST if provided, otherwise 'localhost'
     dialect: 'mysql',
-    port: 3306,
+    port: process.env.PORT || 3306, // Use the PORT variable provided by Heroku, default to 3306 if not available
   });
 }
 
