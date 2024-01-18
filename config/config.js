2a268e0910a5e3a6d8cd936fa2c7da6f382c3299
Wolffkran <liebherr2008@live.com>
2024-01-18 00:02:48 -0500

    .

--- config/config.js
+++ config/config.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+  // For Heroku with JawsDB
   sequelize = new Sequelize(process.env.JAWSDB_URL, {
     dialect: 'mysql',
     dialectOptions: {
@@ -12,12 +13,13 @@ if (process.env.JAWSDB_URL) {
       },
     },
   });
-} /* else {
+} else {
+  // For local development or other environments
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
-    port: 3306,
+    port: process.env.DB_PORT || 3306,
   });
-} */
+}
 
 module.exports = sequelize;
