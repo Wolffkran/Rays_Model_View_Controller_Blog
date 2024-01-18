6e41ae959b18a1a7e523ae8b62b98ae78c75ad1c
Wolffkran <liebherr2008@live.com>
2024-01-18 00:23:53 -0500

    change

--- config/config.js
+++ config/config.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  // For Heroku with JawsDB
   sequelize = new Sequelize(process.env.JAWSDB_URL, {
     dialect: 'mysql',
     dialectOptions: {
@@ -14,11 +13,10 @@ if (process.env.JAWSDB_URL) {
     },
   });
 } else {
-  // For local development or other environments
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST || 'localhost',
+    host: 'localhost',
     dialect: 'mysql',
-    port: process.env.DB_PORT || 3306,
+    port: 3306,
   });
 }
 
