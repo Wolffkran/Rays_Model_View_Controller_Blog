67bdcf204cad1822e2aefd118cb5882a37ba1674
Wolffkran <liebherr2008@live.com>
2024-01-20 16:46:31 -0500

    Changed server.js

--- server.js
+++ server.js
@@ -3,19 +3,21 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');  
+const sequelize = require('./config/config');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controllers');
+const userRoutes = require('./controllers/api/user-routes'); // Import user routes
+const postRoutes = require('./controllers/api/post-routes'); // Import post routes
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/config');  
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const sess = {
-  secret: process.env.SESSION_SECRET || 'Super secret secret',
+  secret: process.env.SESSION_SECRET || 'SuperSecretSecret', 
   cookie: {
     maxAge: 300000,
     httpOnly: true,
-    secure: true, // Set to true if using HTTPS
+    secure: process.env.NODE_ENV === 'production', // Set to true if using HTTPS in production
     sameSite: 'strict',
   },
   resave: false,
@@ -36,7 +38,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers'));  
+app.use('./controllers/api/user-routes', userRoutes); 
+app.use('./controllers/api/post-routes', postRoutes); 
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
