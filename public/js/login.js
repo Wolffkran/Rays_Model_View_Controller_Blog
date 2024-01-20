a2edbd15fd763bb457b08e398aa8101a89845f64
Wolffkran <liebherr2008@live.com>
2024-01-20 16:23:31 -0500

    Dom

--- public/js/login.js
+++ public/js/login.js
@@ -1,32 +1,34 @@
-const loginFormHandler = async function(event) {
-  event.preventDefault();
+document.addEventListener('DOMContentLoaded', function () {
+  const loginFormHandler = async function (event) {
+    event.preventDefault();
 
-  const usernameEl = document.querySelector('#username-input-login');
-  const passwordEl = document.querySelector('#password-input-login');
+    const usernameEl = document.querySelector('#username-input-login');
+    const passwordEl = document.querySelector('#password-input-login');
 
-  try {
-    const response = await fetch('/api/user/login', {
-      method: 'POST',
-      body: JSON.stringify({
-        username: usernameEl.value,
-        password: passwordEl.value,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    });
+    try {
+      const response = await fetch('/api/user/login', {
+        method: 'POST',
+        body: JSON.stringify({
+          username: usernameEl.value,
+          password: passwordEl.value,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+      });
 
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      const errorMessage = await response.text();
-      console.error('Failed to login:', errorMessage);
-      alert(`Failed to login. ${errorMessage}`);
+      if (response.ok) {
+        window.location.replace('/dashboard');
+      } else {
+        const errorMessage = await response.text();
+        console.error('Failed to login:', errorMessage);
+        alert(`Failed to login. ${errorMessage}`);
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      alert('An unexpected error occurred. Check the console for details.');
     }
-  } catch (error) {
-    console.error('Error during login:', error);
-    alert('An unexpected error occurred. Check the console for details.');
-  }
-};
+  };
 
-document
-  .querySelector('#login-form')
-  .addEventListener('submit', loginFormHandler);
+  document
+    .querySelector('#login-form')
+    .addEventListener('submit', loginFormHandler);
+});
